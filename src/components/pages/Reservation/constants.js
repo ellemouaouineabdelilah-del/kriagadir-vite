d2fedaf76100e833b4a6864d0d79ec08eca759c1
1Bitif <aajliabdellatif@gmail.com>
2026-06-08 12:05:17 +0100

    add new file for reserv

---
+++ src/components/pages/Reservation/constants.js
@@ -0,0 +1,24 @@
+// Mock car data (replace with API data later)
+export const availableCars = [
+  { id: 1, name: "Tesla Model 3", pricePerDay: 120, caution: 800 },
+  { id: 2, name: "BMW Série 3", pricePerDay: 95, caution: 700 },
+  { id: 3, name: "Mercedes Classe C", pricePerDay: 110, caution: 750 },
+  { id: 4, name: "Audi A4", pricePerDay: 100, caution: 720 },
+  { id: 1, name: "Tesla Model 3", pricePerDay: 120, caution: 800 },
+  { id: 2, name: "BMW Série 3", pricePerDay: 95, caution: 700 },
+  { id: 3, name: "Mercedes Classe C", pricePerDay: 110, caution: 750 },
+  { id: 4, name: "Audi A4", pricePerDay: 100, caution: 720 },
+  { id: 1, name: "Tesla Model 3", pricePerDay: 120, caution: 800 },
+  { id: 2, name: "BMW Série 3", pricePerDay: 95, caution: 700 },
+  { id: 3, name: "Mercedes Classe C", pricePerDay: 110, caution: 750 },
+  { id: 4, name: "Audi A4", pricePerDay: 100, caution: 720 },
+];
+
+// Delivery/pickup locations
+export const locations = [
+  "Aéroport Agadir Al Massira",
+  "Gare routière d’Agadir",
+  "Hôtel Atlas Amadil Beach",
+  "Centre-ville d’Agadir",
+  "Marina d’Agadir",
+];
